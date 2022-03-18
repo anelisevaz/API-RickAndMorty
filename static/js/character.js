31a4a7441b9b3d6f4052c4a082633a9b6b2644a1
anelisevaz.anelise <anevaz1@gmail.com>
2022-03-17 22:43:58 -0300

    Continuando os ajustes

--- static/js/character.js
+++ static/js/character.js
@@ -32,9 +32,16 @@ return `
        <h1 class="character-name-human">${this.name}</h1>
       </div>
     `}
-    else{
+    else if(this.species == "Alien"){
 return `
         <div class="character-name-alien">
+        <h1>${this.name}</h1>
+      </div>
+    `
+  }
+  else{
+return `
+        <div class="character-name-human">
         <h1>${this.name}</h1>
       </div>
     `
@@ -42,23 +49,58 @@ return `
   
   
   buildDescription() {
-if(this.species == "Human" && this.status == "Alive"){
+if(this.species == "Human"){
 return `
-        <p class="character-species">Espécie: ${this.species}</p>
-        <p class="character-gender">Genero: ${this.gender}</p>
-        <p class="character-type">Tipo: ${this.type}</p>
-        <p class="character-status">Status: ${this.status}</p>
+        <p class="character-species-human">Espécie: ${this.species}</p>
+        <p class="character-gender-human">Genero: ${this.gender}</p>
+        <p class="character-type-human">Tipo: ${this.type}</p>
+        <p class="character-status-human">Status: ${this.status}</p>
       
 `}
-else if(this.species == "Human" && this.status == "Dead"){
+
+else if(this.species == "Humanoid" ){
 return `
-        <p class="character-species">Espécie: ${this.species}</p>
-        <p class="character-gender">Genero: ${this.gender}</p>
-        <p class="character-type">Tipo: ${this.type}</p>
-        <p class="character-status">Status: ${this.status}</p>       
+        <p class="character-species-alihu">Espécie: ${this.species}</p>
+        <p class="character-gender-alihu">Genero: ${this.gender}</p>
+        <p class="character-type-alihu">Tipo: ${this.type}</p>
+        <p class="character-status-alihu">Status: ${this.status}</p>       
         `}
 
-else{
+else if(this.species == "Alien" ){
+return `
+        <p class="character-species-alihu">Espécie: ${this.species}</p>
+        <p class="character-gender-alihu">Genero: ${this.gender}</p>
+        <p class="character-type-alihu">Tipo: ${this.type}</p>
+        <p class="character-status-alihu">Status: ${this.status}</p>       
+        `}
+        
+else if(this.species == "Animal" || this.species == "Mythological Creature" ){
+return `
+        <p class="character-species-animyth">Espécie: ${this.species}</p>
+        <p class="character-gender-animyth">Genero: ${this.gender}</p>
+        <p class="character-type-animyth">Tipo: ${this.type}</p>
+        <p class="character-status-animyth">Status: ${this.status}</p>       
+        `}
+        
+else if(this.species == "Cronenberg" || this.species == "Disease" ){
+return `
+        <p class="character-species-disease">Espécie: ${this.species}</p>
+        <p class="character-gender-disease">Genero: ${this.gender}</p>
+        <p class="character-type-disease">Tipo: ${this.type}</p>
+        <p class="character-status-disease">Status: ${this.status}</p>       
+        `}
+        
+                
+else if(this.species == "Poopybutthole" || this.species == "Robot" ){
+return `
+        <p class="character-species-robot">Espécie: ${this.species}</p>
+        <p class="character-gender-robot">Genero: ${this.gender}</p>
+        <p class="character-type-robot">Tipo: ${this.type}</p>
+        <p class="character-status-robot">Status: ${this.status}</p>       
+        `}
+
+
+else{ //unknown
 return `
         <p class="character-species">Espécie: ${this.species}</p>
         <p class="character-gender">Genero: ${this.gender}</p>
@@ -67,20 +109,7 @@ return `
               `}
   }
  
-     
-	/*	Espécies:
-			unknown
-			Alien
-			Human
-			Poopybutthole - espécie fantasma quadridimensional, Ele sobrevive de vidas semelhantes às que os caranguejos eremitas usam para roubar outras conchas
-			Mythological Creature
-			Animal
-			Robot
-			Cronenberg - A Mosca - iamgem
-			Humanoid
-			Disease  -doença
-      */
-
+  
   
   render() {
     this.$characterImageContainer.innerHTML = this.buildImage()
